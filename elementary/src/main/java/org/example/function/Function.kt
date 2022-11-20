41a81435c0eb8992b0bb78d83f4d4d35012928c6
ilya <ishabelnik@unislabs.com>
2022-11-20 12:34:05 +0300

    Вызов функций котлин в java.

---
+++ elementary/src/main/java/org/example/function/Function.kt
@@ -0,0 +1,6 @@
+@file:JvmName("Function")
+package org.example.function
+
+import java.util.*
+
+fun randomInt() : Int = Random().nextInt();
---
+++ elementary/src/main/java/org/example/function/Main.java
@@ -0,0 +1,13 @@
+package org.example.function;
+
+/**
+ * @author Shabelnik Ilya (ishabelnik@unislabs.com)
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        int randomInt = Function.randomInt();
+        System.out.println(randomInt);
+    }
+}
